c6254e4741d8b621f3dbf89fb04e91b254627b27
Nguyen Dong Thuc <nguyendongthuc13@gmail.com>
2022-02-01 18:56:01 +0700

    Update: Apply custom function to verify TC and gives detail log

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -3,6 +3,8 @@ package commons;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,6 +12,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.opera.OperaDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -18,6 +22,11 @@ public class BaseTest {
 	protected String projectPath = System.getProperty("user.dir");
 	private String chromeVersion = "93.0.4577.63";
 	protected String userUrl,adminUrl;
+	protected final Log log;
+	
+	protected BaseTest() {
+		log=LogFactory.getLog(getClass());
+	}
 	
 	protected WebDriver getLocalBrowserDriver(String browserName) {
 		if (browserName.equals("chrome")){
@@ -92,7 +101,7 @@ public class BaseTest {
 		else {
 			throw new RuntimeException("Browser name invalid");
 		}
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(GlobalConstants.LONG_TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		return driver;
 	}
@@ -106,5 +115,67 @@ public class BaseTest {
 			default:
 				System.out.println("Wrong environment name");
 		}
+	}
+	
+	private boolean checkTrue(boolean condition) {
+		boolean pass = true;
+		try {
+			if (condition == true) {
+				log.info(" -------------------------- PASSED -------------------------- ");
+			} else {
+				log.info(" -------------------------- FAILED -------------------------- ");
+			}
+			Assert.assertTrue(condition);
+		} catch (Throwable e) {
+			pass = false;
+
+			// Add lỗi vào ReportNG
+			VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+			Reporter.getCurrentTestResult().setThrowable(e);
+		}
+		return pass;
+	}
+
+	protected boolean verifyTrue(boolean condition) {
+		return checkTrue(condition);
+	}
+
+	private boolean checkFailed(boolean condition) {
+		boolean pass = true;
+		try {
+			if (condition == false) {
+				log.info(" -------------------------- PASSED -------------------------- ");
+			} else {
+				log.info(" -------------------------- FAILED -------------------------- ");
+			}
+			Assert.assertFalse(condition);
+		} catch (Throwable e) {
+			pass = false;
+			VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+			Reporter.getCurrentTestResult().setThrowable(e);
+		}
+		return pass;
+	}
+
+	protected boolean verifyFalse(boolean condition) {
+		return checkFailed(condition);
+	}
+
+	private boolean checkEquals(Object actual, Object expected) {
+		boolean pass = true;
+		try {
+			Assert.assertEquals(actual, expected);
+			log.info(" -------------------------- PASSED -------------------------- ");
+		} catch (Throwable e) {
+			pass = false;
+			log.info(" -------------------------- FAILED -------------------------- ");
+			VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+			Reporter.getCurrentTestResult().setThrowable(e);
+		}
+		return pass;
+	}
+
+	protected boolean verifyEquals(Object actual, Object expected) {
+		return checkEquals(actual, expected);
 	}
 }
---
+++ testcases/com/facebook/TC_AssertAndVerify.java
@@ -0,0 +1,35 @@
+package com.facebook;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+
+public class TC_AssertAndVerify extends BaseTest {
+	WebDriver driver;
+	RegisterPageObject registerPage;
+	
+	@Parameters({"browser","url"})
+	@BeforeClass
+	public void beforeClass(String browserName, String url) {
+		driver = getBrowserDriver(browserName);	
+		registerPage = PageGeneratorManager.getRegisterPage(driver);
+		registerPage.openBrowser(driver, url);
+	}
+	
+	@Test
+	public void TC_01_Assert() {	
+		registerPage.clickRegisterLink();
+		verifyTrue(registerPage.isEmailTextboxDisplayed());
+		verifyTrue(registerPage.isConfirmEmailTextboxDisplayed());
+		verifyFalse(registerPage.isLoginButtonNotDisplayed());
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
